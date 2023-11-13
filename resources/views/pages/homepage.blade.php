cf00914b47070acafdd01683a20474825b17729d
Devan Janssen <devanjanssen@gmail.com>
2023-11-13 14:41:31 +0100

    added navbar

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Titel</h5>
+                <p class="card-text">Download Park Planner. De nummer 1 parkeer app</p>
+                <a href="#" class="btn btn-primary">Download App</a>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/homepage', [HomepageController::class, 'index']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
